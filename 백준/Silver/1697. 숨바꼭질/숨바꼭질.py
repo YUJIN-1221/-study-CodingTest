c4fa482f26da71d4d919eb71a903b48e62226248
YUJIN-1221 <78401311+YUJIN-1221@users.noreply.github.com>
2022-11-18 10:08:08 +0900

    [Silver I] Title: 숨바꼭질, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,12 +1,12 @@
-def c(n,k):
-    if n>=k:
+def rec(n,k):
+    if n >= k:
         return n-k
     elif k == 1:
         return 1
-    elif k%2:
-        return 1+min(c(n,k-1),c(n,k+1))
+    elif k % 2:
+        return 1+min(rec(n, k-1), rec(n, k+1))
     else:
-        return min(k-n, 1+c(n,k//2))
+        return min(k-n, 1 + rec(n, k//2))
     
-n, k = map(int,input().split())
-print(c(n,k))
+n, k = map(int, input().split())
+print(rec(n, k))
