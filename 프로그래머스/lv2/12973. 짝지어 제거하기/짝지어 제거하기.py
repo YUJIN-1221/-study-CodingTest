47338ea040cd3a7bacab5a013476481c0530bca3
yj-yujin-lee <78401311+yj-yujin-lee@users.noreply.github.com>
2023-09-08 10:48:33 +0900

    [level 2] Title: 짝지어 제거하기, Time: 137.50 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    
+    stack = []
+    for i in range(len(s)):
+        if stack == []:
+            stack.append(s[i])
+        else:
+            if stack[-1] == s[i]:
+                stack.pop(-1)
+            else:
+                stack.append(s[i])
+    return int(stack == [])
