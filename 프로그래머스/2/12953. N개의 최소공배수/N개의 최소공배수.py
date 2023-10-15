f6facc17164ab3358deae173af54c59ad71fad02
yj-yujin-lee <78401311+yj-yujin-lee@users.noreply.github.com>
2023-10-15 23:45:52 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    import math
+    
+    for k in range(len(arr)-1):
+        gcd = math.gcd(arr[k], arr[k+1])
+        lcm = arr[k] * arr[k+1] // gcd
+        arr[k+1] = lcm
+            
+    return lcm
